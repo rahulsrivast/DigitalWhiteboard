6c185945ca590a70683807dbfd5dc8210aa4194f
rahulsrivast <rahul.srivast@gmail.com>
2018-07-29 14:07:46 +0530

    Digital Whiteboard Project checkin
    Digital Whiteboard project files

---
+++ DigitalWhiteboard/MainPage.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DigitalWhiteboard
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            // Load the whiteboard
+            Whiteboard.Instance.Initialize();
+        }
+
+        public void AgendaStackPanel_AgendaViewLoaded(object sender, RoutedEventArgs e)
+        {
+            // Convert to StackPanel
+            StackPanel s = (StackPanel)sender;
+
+            // Loop through the Agenda data structure and add TextBlocks
+            foreach (var day in Whiteboard.Instance.agenda.days)
+            {
+                // For each day, add the date
+                TextBlock dateTB = new TextBlock();
+                dateTB.Text = "\n" + day.Key.Date.DayOfWeek.ToString() + ", " + day.Key.Date.Month.ToString() + "/" + day.Key.Date.Day.ToString();
+
+                // Add to stack panel
+                s.Children.Add(dateTB);
+
+                // Loop through events on the day and add to the agenda view
+                foreach (var cevent in day.Value.events)
+                {
+                    // For each event, add time and title
+                    // For each day, add the date
+                    Button timeTB = new Button();
+                    timeTB.Content = cevent.startTime.TimeOfDay.ToString() + " - " + cevent.endTime.TimeOfDay.ToString() + "\n" + cevent.title;
+
+                    // Add to stack panel
+                    s.Children.Add(timeTB);
+                }
+            }
+        }
+
+        public void NotesStackPanel_NotesViewLoaded(object sender, RoutedEventArgs e)
+        {
+            // Convert to StackPanel
+            StackPanel s = (StackPanel)sender;
+
+            // Loop through the Agenda data structure and add TextBlocks
+            foreach (var note in Whiteboard.Instance.notes)
+            {
+                // < Border BorderThickness = "2" BorderBrush = "#FFFFD700" Background = "#FFFFFFFF" >
+                // Add textblock for each note and add a click callback
+                Border b = new Border();
+                b.BorderBrush = new SolidColorBrush(Colors.Black);
+                Thickness bt = new Thickness(1.0);
+                b.BorderThickness = bt;
+                TextBlock tb = new TextBlock();
+                tb.Text = note.text + "\n" + "Reminder: " + note.reminder.TimeOfDay.ToString() + " - " + note.reminder.TimeOfDay.ToString();
+                b.Child = tb;
+                s.Children.Add(b);
+            }
+        }
+
+        // Connect to the account
+        public void ConnectButton_ButtonClicked(object sender, RoutedEventArgs e)
+        {
+            Button ConnectButton = (Button)sender;
+            if (await SignInCurrentUserAsync())
+            {
+                ConnectButton.Content = "Disconnect";
+            }
+        }
+
+        /// <summary>
+        /// Signs in the current user.
+        /// </summary>
+        /// <returns></returns>
+        public async bool SignInCurrentUserAsync()
+        {
+            var token = await AuthenticationHelper.GetTokenForUserAsync();
+
+            if (token == null)
+            {
+                //
+                return false;
+            }
+
+            // Fetch the Notes list
+
+            // Fetch all the notes from the notes list
+
+            // 
+        }
+    }
+}
---
+++ DigitalWhiteboard/Whiteboard.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalWhiteboard
+{
+    // signed-in user data for Outlook, WunderList and Cortana
+    public class User
+    {
+        public User()
+        {
+            // Outlook Calendar Data
+
+            // Login to outlook if not already done
+
+            // Pull calendar data from outlook
+     
+            // Login to wunderlist if not already done
+        }
+    }
+
+    // Calendar event class containing calendar meetings and events
+    public class CalendarEvent
+    {
+        // Title of the event
+        public String title;
+
+        // Start Time of the event
+        public DateTime startTime;
+
+        // End Time of the event  
+        public DateTime endTime;
+
+        public CalendarEvent(String titleArg, DateTime startTimeArg, DateTime endTimeArg)
+        {
+            title = titleArg;
+            startTime = startTimeArg;
+            endTime = endTimeArg;
+        }
+    }
+
+    // AgendaDay class for showing events for a particular day
+    public class AgendaDay
+    {
+        // Contains an array of calendar events
+        public List<CalendarEvent> events;
+
+        // Contains the date represented by the AgendaDay
+        public DateTime day;
+
+        public AgendaDay()
+        {
+            events = new List<CalendarEvent>();
+        }
+    }
+
+    // Agenda class that contains a chronological view of calendar events
+    public class Agenda
+    {
+        // Create an array of Agenda Days sorted by day
+        public SortedList<DateTime, AgendaDay> days;
+
+        // When agenda view initializes, it loads the calendar events from all the calendars associated with the logged-in user
+        public Agenda()
+        {
+            // Initialize days list
+            days = new SortedList<DateTime, AgendaDay>();
+
+            // Fetch all calendar events from all calendars of the signed-in user
+            FetchAllCalendarEvents();
+            
+            // 
+        }
+
+        // Fetches the calendar events using Outlook REST API and populates the 'days' variable
+        public void FetchAllCalendarEvents()
+        {
+            // Login to Outlook
+
+            // Fetch the calendar events and add to the sorted list
+            // Add one for 1 Sep 2018, 1 PM - 2 PM
+            CalendarEvent ce = new CalendarEvent("test event 1", new DateTime(2018, 9, 1, 13, 0, 0), new DateTime(2018, 9, 1, 14, 0, 0));
+            AgendaDay ad = new AgendaDay();
+            ad.events.Add(ce);
+            days.Add(new DateTime(2018, 9, 1), ad);
+
+            // Add one for 3 Sep 2018, 5 PM - 7 PM
+            ce = new CalendarEvent("test event 2", new DateTime(2018, 9, 3, 17, 0, 0), new DateTime(2018, 9, 3, 18, 0, 0));
+            ad = new AgendaDay();
+            ad.events.Add(ce);
+            days.Add(new DateTime(2018, 9, 3), ad);
+
+        }
+    }
+
+    // Note
+    public class Note
+    {
+        // text portion of the note
+        public String text;
+
+        // reminder on the note
+        public DateTime reminder;
+    }
+
+    // Bad code! Do not use!
+    public sealed class Whiteboard
+    {
+        private static Whiteboard instance = null;
+
+        public Agenda agenda;
+        public User user;
+        public List<Note> notes;
+        public Boolean firstFetch;
+
+        private Whiteboard()
+        {
+            
+        }
+
+        public void Initialize()
+        {
+            // Create the registered user and login to WunderList, Outlook and Cortana
+            user = new User();
+
+            // Create the agenda class            
+            agenda = new Agenda();
+
+            // Create the notes class
+            notes = new List<Note>();
+
+            // Fetches all notes
+            firstFetch = false;
+            FetchNotes();
+        }
+
+        public void FetchNotes()
+        {
+            // Connect to WunderList
+
+            // Fetch the Notes list
+
+            // Add all notes from the list into notes
+
+            // Note 1: text - test, reminder - 1st Sep, 2018 | 3 pm
+            Note n = new Note();
+            n.text = "test"; n.reminder = new DateTime(2018, 9, 1, 15, 0, 0);
+            notes.Add(n);
+
+            // Note 2: text - test2, reminder - 10th Sep, 2018 | 5 pm
+            n = new Note();
+            n.text = "test2"; n.reminder = new DateTime(2018, 9, 10, 17, 0, 0);
+            notes.Add(n);
+
+            firstFetch = true;
+        }
+
+        public static Whiteboard Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Whiteboard();
+                }
+                return instance;
+            }
+        }
+    }
+}
